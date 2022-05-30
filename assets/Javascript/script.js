9f2495f757f7620e4fcab211318116f884cb24a7
GSiggins <gregory.siggins@gmail.com>
2022-05-30 15:08:10 -0700

    added comments to explain functions

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -1,25 +1,23 @@
-// Assignment Code
+// Defining global variables
 var generateBtn = document.querySelector("#generate");
 var possibleChars = []
 var passArray = []
+var finalArray = []
 
-
-
+//Starting initial function
 passwordGenerate()
 
 function passwordGenerate() {
-
- 
+//Establishing arrays of possible characters and defining them as variables
   var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
   var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
   var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
   var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '+', '-', '=', '~']
 
-
-
   validateUserInput()
+  // Establishing parameters for prompt. Must be 8-128. Must be numbers.
   function validateUserInput() {
-    let userLength = parseInt(prompt("How long do you want your password? (Between 8-128)"),10);
+    let userLength = parseInt(prompt("How long do you want your password? (Between 8-128)"), 10);
     if (!userLength) {
       alert("Please type a number between 8-128");
       validateUserInput();
@@ -30,15 +28,16 @@ function passwordGenerate() {
       validateUserInput();
       return null
     }
-   
-    userPrompts()
 
+    userPrompts()
+    //Confirming character types via boolean referencing the array.
     function userPrompts() {
       var includeUpper = confirm("Do you want to include uppercase letters? OK = Yes. Cancel = No.");
       var includeLower = confirm("Do you want to include lowercase letters? OK = Yes. Cancel = No.");
       var includeNumber = confirm("Do you want to include numbers? OK = Yes. Cancel = No.");
       var includeSpecial = confirm("Do you want to include special characters? OK = Yes. Cancel = No.");
 
+      //Creates exception if user chooses no characters at all
       if (!includeLower && !includeNumber && !includeSpecial && !includeUpper) {
         alert("You need to select at least one set of characters!");
         userPrompts();
@@ -52,8 +51,10 @@ function passwordGenerate() {
       console.log(includeSpecial)
       console.log(userLength)
 
-      generatePassword()
-      function generatePassword() {
+      //Concatonates the master array with user-confirmed character types
+      generateArray()
+
+      function generateArray() {
         var possibleChars = []
         if (includeUpper) {
           var possibleChars = possibleChars.concat(upperCase);
@@ -74,13 +75,23 @@ function passwordGenerate() {
         console.log(possibleChars)
         return
       }
-
-      randomSelect()
-     
-        }
-        return;
-      }
     }
+    return;
+  }
+
+  arrayLoop()
+
+  function arrayLoop(userLength, possibleChars) {
+
+    while (finalArray.length < userLength) {
+      var itemIndex = Math.floor((Math.random) * possibleChars.length)
+      finalArray.push(possibleChars[itemIndex])
+      console.log(finalArray)
+      return;
+    }
+  }
+
+}
 
 // function writePassword() {
 // var password = generatePassword();
